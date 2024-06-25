1a50159b755acf99074d9e49fee40813def85e4f
Arty <daltonhberg@icloud.com>
2024-06-24 23:20:56 -0700

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+    <div className="alert alert-primary">
+      <Alert>
+        Hello <span style={{ color: "green" }}>World</span>
+      </Alert>
     </div>
   );
 }
