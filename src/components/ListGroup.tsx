537c940ef33b6c2e7fd14984da14900bf21089be
Arty <daltonhberg@icloud.com>
2024-06-24 21:51:43 -0700

    progress

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,34 @@
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Parris"];
+import { useState } from "react";
+// { items: [], heading: string}
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  // (item : string) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1); // updater function
+
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={(event) => console.log(event)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+            style={{ cursor: "pointer" }}
           >
             {item}
           </li>
