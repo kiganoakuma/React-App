2d505b58f1272960ef747718e2d38e5bbb67addc
Arty <daltonhberg@icloud.com>
2024-06-24 17:49:00 -0700

    test on phone

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Parris"];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found.</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
