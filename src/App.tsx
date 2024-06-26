5e0e2960fffae9eb918773a03044441a7692890f
kiganaoakuma <daltonhberg@icloud.com>
2024-06-26 13:42:26 -0700

    updating objects using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import Like from "./components/Like/Like";
+import { useState } from "react";
+import produce from "immer";
+import Button from "./components/Button/Button";
 
 function App() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleClick = () => {
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug1 = draft.find((bug) => bug.id === 1);
+        const bug2 = draft.find((bug) => bug.id === 2);
+        if (bug1) bug1.fixed = true;
+        if (bug2) bug2.fixed = true;
+      })
+    );
+  };
   return (
     <div>
-      <Like onClick={() => console.log("clicked")}></Like>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "new"}
+        </p>
+      ))}
+      <Button onClick={handleClick}>Click Me!</Button>
     </div>
   );
 }
