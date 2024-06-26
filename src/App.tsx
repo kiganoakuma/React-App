207fa6647b5ec87889fe387bd1b6646d8f353d36
kiganaoakuma <daltonhberg@icloud.com>
2024-06-26 15:39:54 -0700

    sharing useState between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
 import produce from "immer";
-import Button from "./components/Button/Button";
+import NavBar from "./components/navBar";
+import Cart from "./components/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
-  const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setBugs(
-      produce((draft) => {
-        const bug1 = draft.find((bug) => bug.id === 1);
-        const bug2 = draft.find((bug) => bug.id === 2);
-        if (bug1) bug1.fixed = true;
-        if (bug2) bug2.fixed = true;
-      })
-    );
-  };
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "new"}
-        </p>
-      ))}
-      <Button onClick={handleClick}>Click Me!</Button>
+      <NavBar cartItemsCount={cartItems.length}></NavBar>
+      <Cart onClear={() => setCartItems([])} cartItems={cartItems}></Cart>
     </div>
   );
 }
