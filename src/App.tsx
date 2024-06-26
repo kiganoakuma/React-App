a23c9c100c84399a824d6fba32f1c35caf037669
kiganaoakuma <daltonhberg@icloud.com>
2024-06-26 15:55:56 -0700

    update useState Exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
-import produce from "immer";
-import NavBar from "./components/navBar";
-import Cart from "./components/Cart";
 
-function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+const App = () => {
+  interface Props {}
 
-  return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart onClear={() => setCartItems([])} cartItems={cartItems}></Cart>
-    </div>
-  );
-}
+  const Cart = ({}: Props) => {
+    const [game, setGame] = useState({
+      id: 1,
+      player: {
+        name: "John",
+      },
+    });
+
+    const handleClick = () => {
+      setGame({ ...game, player: { ...game.player, name: "Bob" } });
+    };
+
+    return <div></div>;
+  };
+};
 
 export default App;
