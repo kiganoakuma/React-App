e015d5c96e345230e6d27759eaa3d18fff872b44
Arty <daltonhberg@icloud.com>
2024-07-28 13:56:32 -0700

    finished user-services.ts and implementation

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -11,8 +11,19 @@ class UserServices {
     const request = apiClient.get<User[]>("/users", {
       signal: controller.signal,
     });
-    const delete = apiClient.delete("/users/" + user.id)
     return { request, cancel: () => controller.abort() };
+  }
+
+  deleteUser(id: number) {
+    return apiClient.delete("/users/" + id);
+  }
+
+  createUser(user: User) {
+    return apiClient.post("/users", user);
+  }
+
+  updateUser(id: number, user: User) {
+    return apiClient.patch("/users/" + id, user);
   }
 }
 
